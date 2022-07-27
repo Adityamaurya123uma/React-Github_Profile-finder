f4ca07da45a4a164e527cb54c8c3c44d4d679ff0
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-07-27 22:33:04 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import User from "./components/users/Users";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 import axios from "axios";
 import "./App.css";
 
@@ -13,25 +15,56 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
-  async componentDidMount() {
-    //console.log(REACT_APP_GITHUB_CLEINT_SECRET);
-    this.setState({ loading: true });
+  // async componentDidMount() {
+  //   //console.log(REACT_APP_GITHUB_CLEINT_SECRET);
+  //   this.setState({ loading: true });
 
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${REACT_APP_GITHUB_CLEINT_ID}&client_secret=${REACT_APP_GITHUB_CLEINT_SECRET}`
+  //   );
+
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // Search github Users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    //console.log(text);
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${REACT_APP_GITHUB_CLEINT_ID}&client_secret=${REACT_APP_GITHUB_CLEINT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${REACT_APP_GITHUB_CLEINT_ID}&client_secret=${REACT_APP_GITHUB_CLEINT_SECRET}`
     );
 
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  //Clear Users form State
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
+  //Setting the alert function
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
 
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <User loading={this.state.loading} users={this.state.users} />
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <User loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,17 +1,55 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter soomething", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
+  };
+
+  onChange = (e) => {
+    this.setState({ text: e.target.value });
+  };
+
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
-        <form className="from">
-          <input type="text" name="text" placeholder="Search User..." />
+        <form onSubmit={this.onSubmit} className="from">
+          <input
+            type="text"
+            name="text"
+            placeholder="Search User..."
+            value={this.state.text}
+            onChange={this.onChange}
+          />
           <input
             type="submit"
             value="Search"
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
